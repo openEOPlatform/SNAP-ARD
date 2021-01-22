8938ff26a80efb6771b30eb6f74a8b71863f6e69
Luca Foresta <luca.foresta@eodc.eu>
2021-01-22 13:50:23 +0100

    cleanup

--- test_geocode_shell.sh
+++ test_geocode_shell.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-INFILE=/home/luca/eodc/data/copernicus.eu/s1a_csar_grdh_iw/2016/08/03/S1A_IW_GRDH_1SDH_20160803T170701_20160803T170731_012439_0136C6_907F.zip
-OUTDIR=/home/luca/eodc/repos/openeo/SNAP-ARD/snap_output2
+INFILE=S1A_IW_GRDH_1SDH_20160803T170701_20160803T170731_012439_0136C6_907F.zip
+OUTDIR=/path/to/outputdir
 SHAPEFILE="{'xmin':9.65,'xmax':13.77,'ymin':45.35,'ymax':47.78}"
-EXTERNALDEMFILE=/home/luca/eodc/repos/openeo/SNAP-ARD/south_tyrol.tif
+EXTERNALDEMFILE=/path/to/south_tyrol.tif
 
 python3 geocode_shell.py $INFILE $OUTDIR $SHAPEFILE $EXTERNALDEMFILE
