86943e3970d55c92837dbbb9c7ad9710df6f8ff3
Luca Foresta <luca.foresta@eodc.eu>
2021-01-22 13:24:05 +0100

    embed projection (global) for snap

--- geocode.py
+++ geocode.py
@@ -7,18 +7,23 @@ from pyroSAR import snap
 import shutil
 
 
-def geocode(infile, outdir, t_srs, tr, shapefile, externalDEMFile=None,
+def geocode(infile, outdir, shapefile, externalDEMFile=None,
             refarea='gamma0', terrainFlattening=True):
 
     identifier = os.path.splitext(os.path.split(infile)[1])[0]
     tmp_dir = "/tmp/" + identifier
     os.makedirs(tmp_dir)
 
+    # Use this projection for SNAP, before reprojectiing to EQUI7
+    # Note: SNAP does not support EQUI7 yet
+    epsg = 54032  # World Azimuthal Equidistant (https://epsg.io/54032)
+    sampling = 10  # in meters
+
     _ = snap.geocode(infile=infile,
                      outdir=tmp_dir,
                      refarea=refarea,
-                     t_srs=t_srs,
-                     tr=tr,
+                     t_srs=epsg,
+                     tr=sampling,
                      shapefile=shapefile,
                      externalDEMFile=externalDEMFile,
                      terrainFlattening=terrainFlattening,
@@ -26,13 +31,11 @@ def geocode(infile, outdir, t_srs, tr, shapefile, externalDEMFile=None,
                      scaling='db', groupsize=1, removeS1ThermalNoise=True
                      )
 
-    file_list = glob(tmp_dir + "/*.tif")
     # Reproject and tile to EQUI7
+    file_list = glob(tmp_dir + "/*.tif")
     e7 = equi7grid.Equi7Grid(sampling=10)
-
     for this_file in file_list:
         _ = image2equi7grid(e7, image=this_file,
                             output_dir=outdir,
                             gdal_path="/usr/bin")
-    # remove temporary dir
     shutil.rmtree(tmp_dir)
