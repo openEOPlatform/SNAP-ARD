de2202fa8a847545f6b39f4ab07d70ca08ed87db
Luca Foresta <luca.foresta@eodc.eu>
2021-01-25 10:54:46 +0100

    properly format shapefile

--- geocode_shell.py
+++ geocode_shell.py
@@ -1,3 +1,4 @@
+import ast
 from geocode import geocode
 import sys
 
@@ -5,6 +6,10 @@ infile = sys.argv[1]
 outdir = sys.argv[2]
 shapefile = sys.argv[3]
 externalDEMFile = sys.argv[4]
+
+if "{" in shapefile:
+    # This is a dict, convert from str
+    shapefile = ast.literal_eval(shapefile)
 
 _ = geocode(infile, outdir,
             shapefile=shapefile,
